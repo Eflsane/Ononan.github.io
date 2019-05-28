da9e3bb0aa8e6fac42a18389e1ef1f17ce33ddb0
Ononan <48926907+Ononan@users.noreply.github.com>
2019-05-28 14:49:20 +0400

    28

--- js/test_js.js
+++ js/test_js.js
@@ -5,6 +5,15 @@
 
 
 
-function startClick(){
-
+function check(){
+    let che = document.getElementsByClassName("checkb");
+    let checkResult=chekChek(che);
+    console.log(checkResult);
+}
+function chekChek(che){
+    console.log(che);
+    for(let i = 0;i<che.length;i++){
+        if(che[i].checked) return i;
+    }
+    return -1;
 }
